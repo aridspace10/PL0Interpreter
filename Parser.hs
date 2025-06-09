0ca5ca97342b00e028ce67b2b2c8a8f740b8481c
aridspace10 <jackovand27@icloud.com>
2025-06-09 20:40:21 +1000

    Inital commit with PLO constants

---
+++ Parser.hs
@@ -0,0 +1,38 @@
+module Parser where
+
+assign = ":=";
+colon = ":";
+semicolon = ";";
+range = "..";
+lparen = "(";
+rparen = ")";
+lbracket = "[";
+rbracket = "]";
+equal = "=";
+nequal = "!=";
+less = "<";
+greater = ">";
+lesseq = "<=";
+greatereq = ">=";
+plus = "+";
+minus = "-";
+times = "*";
+divides = "/";
+kwBegin = "begin";
+kwCall = "call";
+kwConst = "const";
+kwDo = "do";
+kwElse = "else";
+kwEnd = "end";
+kwIf = "if";
+kwProcedure = "procedure";
+kwRead = "read";
+kwThen = "then";
+kwType = "type";
+kwVar = "var";
+kwWhile = "while";
+kwWrite = "write";
+
+
+
+
