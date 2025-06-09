bf4e9a777819c03211755be1ea9b3ea626bb0d77
aridspace10 <jackovand27@icloud.com>
2025-06-09 20:47:42 +1000

    Imported all the useful stuff from COMP3400

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,6 @@
 module Parser where
+import           Control.Applicative
+import           GHC.Natural              (Natural)
 
 assign = ":=";
 colon = ":";
@@ -33,6 +35,89 @@ kwVar = "var";
 kwWhile = "while";
 kwWrite = "write";
 
+newtype Parser a = P (String -> Maybe (a, String))
 
+instance Functor Parser where
+    fmap :: (a -> b) -> Parser a -> Parser b
+    fmap g pa = do
+      a <- pa
+      return $ g a
 
+instance Applicative Parser where
+    pure :: a -> Parser a
+    pure a = P (\cs -> Just (a,cs))
 
+    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+    pg <*> pa = do
+      g <- pg
+      g <$> pa
+
+instance Monad Parser where
+    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    p >>= f = P $ \cs ->
+        case parse p cs of
+          Nothing        -> Nothing
+          Just (a, str') -> parse (f a) str'
+
+instance Alternative Parser where
+    empty :: Parser a
+    empty = P $ \str -> Nothing
+
+    (<|>) :: Parser a -> Parser a -> Parser a
+    p <|> q = P $ \cs ->
+        case parse p cs of
+          Nothing -> parse q cs
+          mx      -> mx
+
+-- aux function for removing decorator
+parse :: Parser a -> String -> Maybe (a, String)
+parse (P p) cs = p cs
+
+parse :: Parser a -> String -> Maybe (a, String)
+parse (P p) cs = p cs
+
+-- parse one character
+item :: Parser Char
+item = P $ foo
+  where
+    foo (c:cs) = Just $ (c, cs)
+    foo _      = Nothing
+
+-- parse a char c when P c.
+sat :: (Char -> Bool) -> Parser Char
+sat p = do
+    x <- item
+    if p x then return x else empty
+
+-- parse a digit
+digit :: Parser Char
+digit = sat (\x -> elem x ['0'..'9'])
+
+-- parse the character x
+char :: Char -> Parser Char
+char x = sat (== x)
+
+-- parse the string xs
+string :: String -> Parser String
+string []     = return []
+string (x:xs) = (\x xs -> x:xs) <$> (char x) <*> (string xs)
+
+-- parse a natural number
+nat :: Parser Natural
+nat = read <$> (some digit)
+
+-- throw away space
+space :: Parser ()
+space = (\x -> ()) <$> (many $ char ' ')
+
+-- ignore surrounding whitespace
+token :: Parser a -> Parser a
+token pa = do
+    space
+    a <- pa
+    space
+    return a
+
+-- parse a symbol, ignoring whitespace
+symbol :: String -> Parser String
+symbol xs = token $ string xs
