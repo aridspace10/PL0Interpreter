764f7db3d23f073002231f6e65154a68af8f5eea
aridspace10 <jackovand27@icloud.com>
2025-06-14 23:31:17 +1000

    Added a few statement evaluators

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use when" #-}
 module Interpreter where
 import Parser
 import Control.Monad.State
@@ -13,13 +15,26 @@ type Interpreter a = StateT Env (Except String) a
 
 lookupVar :: String -> Interpreter Value
 lookupVar name = do
-  env <- get
-  case Map.lookup name env of
-    Just val -> return val
-    Nothing  -> throwError ("Undefined variable: " ++ name)
+    env <- get
+    case Map.lookup name env of
+        Just val -> return val
+        Nothing  -> throwError ("Undefined variable: " ++ name)
 
 assignVar :: String -> Value -> Interpreter ()
 assignVar name val = do
-  env <- get
-  put (Map.insert name val env)
+    env <- get
+    put (Map.insert name val env)
 
+evalStatement :: Statement -> Interpreter ()
+evalStatement (WriteStatement exp) = do
+    val <- evalExp exp
+    liftIO $ print val
+evalStatement (IfStatement cond stat1 stat2) = do
+    r <- evalCondition
+    if r then evalStatement stat1 else evalStatement stat2
+evalStatement (WhileStatement cond stat) = do
+    r <- evalCondition
+    (if r then (do
+        evalStatement stat
+        evalStatement (WhileStatement cond stat)) 
+    else return ())
