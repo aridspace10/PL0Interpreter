4a405ed4f45da638545edffa06edc789906ff801
aridspace10 <jackovand27@icloud.com>
2025-06-14 22:42:28 +1000

    Added lookup and assign

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,9 +1,25 @@
 module Interpreter where
 import Parser
+import Control.Monad.State
+import Control.Monad.Except
 import qualified Data.Map as Map
 
 type Env = Map.Map String Value
-data Value 
-  = IntVal Int 
-  | BoolVal Bool
+
+data Value = IntVal Int | BoolVal Bool
   deriving (Show, Eq)
+
+type Interpreter a = StateT Env (Except String) a
+
+lookupVar :: String -> Interpreter Value
+lookupVar name = do
+  env <- get
+  case Map.lookup name env of
+    Just val -> return val
+    Nothing  -> throwError ("Undefined variable: " ++ name)
+
+assignVar :: String -> Value -> Interpreter ()
+assignVar name val = do
+  env <- get
+  put (Map.insert name val env)
+
