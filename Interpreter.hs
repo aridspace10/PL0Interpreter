4a0905238b99f43aea2e351d2c8e9036024ccee0
aridspace10 <jackovand27@icloud.com>
2025-06-17 00:05:15 +1000

    Made Value Use a maybe type for declared but not initalised variables

--- Interpreter.hs
+++ Interpreter.hs
@@ -11,7 +11,7 @@ import Foreign.C (throwErrno)
 
 type Env = Map.Map String Value
 
-data Value = IntVal Int | BoolVal Bool
+data Value = IntVal (Maybe Int) | BoolVal (Maybe Bool)
   deriving (Show, Eq)
 
 type Interpreter a = StateT Env (ExceptT String IO) a
@@ -35,16 +35,22 @@ evalStatement (WriteStatement exp) = do
 evalStatement (IfStatement cond stat1 stat2) = do
     val <- evalCondition cond
     case (val) of
-        (BoolVal r) -> if r then evalStatement stat1 else evalStatement stat2
+        (BoolVal r) -> do
+            case (r) of
+                (Just v) -> if v then evalStatement stat1 else evalStatement stat2
+                _ -> return ()
         _ -> throwError ("Big Boy Problem")
 evalStatement (WhileStatement cond stat) = do
     val <- evalCondition cond
     case (val) of
         (BoolVal r) -> do
-            if r then (do 
-                    evalStatement stat
-                    evalStatement (WhileStatement cond stat))
-            else return ()
+            case (r) of 
+                (Just v) -> do
+                    if v then (do 
+                            evalStatement stat
+                            evalStatement (WhileStatement cond stat))
+                    else return ()
+                _ -> return ()
         _ -> throwError ("Big Boy Problem")
 
 evalCondition :: Condition -> Interpreter Value
@@ -53,17 +59,18 @@ evalCondition (RelationalCondition lexp (RelOp op) rexp) = do
   elexp <- evalExp lexp
   erexp <- evalExp rexp
   case (elexp, erexp) of
-    (IntVal l, IntVal r) -> case op of
-      ">"  -> return $ BoolVal (l > r)
-      ">=" -> return $ BoolVal (l >= r)
-      "<"  -> return $ BoolVal (l < r)
-      "<=" -> return $ BoolVal (l <= r)
-      "!=" -> return $ BoolVal (l /= r)
-      "==" -> return $ BoolVal (l == r)
-      _    -> throwError ("Unknown relational operator: " ++ op)
+    (IntVal ml, IntVal mr) -> case (ml, mr) of       
+        (Just l, Just r) -> case op of
+            ">"  -> return $ BoolVal $ Just (l > r)
+            ">=" -> return $ BoolVal $ Just (l >= r)
+            "<"  -> return $ BoolVal $ Just (l < r)
+            "<=" -> return $ BoolVal $ Just (l <= r)
+            "!=" -> return $ BoolVal $ Just (l /= r)
+            "==" -> return $ BoolVal $ Just (l == r)
+            _    -> throwError ("Unknown relational operator: " ++ op)
     (BoolVal l, BoolVal r) -> case op of
-      "==" -> return $ BoolVal (l == r)
-      "!=" -> return $ BoolVal (l /= r)
+      "==" -> return $ BoolVal $ Just (l == r)
+      "!=" -> return $ BoolVal $ Just (l /= r)
       _    -> throwError ("Unsupported operator for boolean values: " ++ op)
     _ -> throwError "Type error in relational condition"
 
@@ -71,19 +78,21 @@ evalExp :: Exp -> Interpreter Value
 evalExp (SingleExp str term) = do
     val <- evalTerm term
     case (val) of
-        (IntVal e) -> do
-            if str == "+" 
-            then return (IntVal e)
-            else return $ IntVal (e * (-1))
+        (IntVal me) -> case (me) of
+            (Just e) -> do
+                if str == "+" 
+                then return (IntVal $ Just e)
+                else return $ IntVal $ Just (e * (-1))
         (BoolVal e) -> throwError ("Big Bad Moment No.1")
 evalExp (BinaryExp str term exp) = do
     eval <- evalTerm term
     eexp <- evalExp exp
     case (eval, eexp) of
-        (IntVal left, IntVal right) -> do
-            if str == "+"
-            then return (IntVal $ left + right)
-            else return (IntVal $ (left * (-1)) + right)
+        (IntVal mleft, IntVal mright) -> case (mleft, mright) of
+            (Just left, Just right) -> do
+                if str == "+"
+                then return (IntVal $ Just $ left + right)
+                else return (IntVal $ Just $ -left + right)
 
 
 evalTerm :: Term -> Interpreter Value
@@ -91,7 +100,7 @@ evalTerm (SingleFactor fact) = evalFactor fact
 
 evalFactor :: Factor -> Interpreter Value
 evalFactor (FactorLValue lval) = evalLValue lval
-evalFactor (FactorNumber num) = return (IntVal $ fromIntegral num)
+evalFactor (FactorNumber num) = return (IntVal $ Just $ fromIntegral num)
 evalFactor (FactorParen cond) = do evalCondition cond
 
 evalIdentifier :: Identifier -> Interpreter Value
@@ -102,4 +111,4 @@ evalLValue :: LValue -> Interpreter Value
 evalLValue (LValue x) = evalIdentifier x
 
 testEnv :: Env
-testEnv = Map.fromList [("x", IntVal 5), ("y", BoolVal True)]
+testEnv = Map.fromList [("x", IntVal $ Just 5), ("y", BoolVal $ Just True)]
