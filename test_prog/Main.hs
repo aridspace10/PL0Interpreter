07be903a97a9d0f5ea3a38e594c2dda6a96ff4a3
aridspace10 <jackovand27@icloud.com>
2025-07-21 11:52:10 +1000

    Added More Imports

--- test_prog/Main.hs
+++ test_prog/Main.hs
@@ -2,6 +2,11 @@ module Main where
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import System.IO.Silently (capture)
+import System.Directory (listDirectory)
+import System.FilePath ((</>))
+import Control.Monad (forM)
+
 
 main :: IO ()
 main = defaultMain tests
@@ -9,5 +14,7 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "PL0 Tests"
   [ testCase "Basic sanity check" $
+      2 + 2 @?= 4,
+    testCase "Basic sanity check" $
       2 + 2 @?= 4
   ]
